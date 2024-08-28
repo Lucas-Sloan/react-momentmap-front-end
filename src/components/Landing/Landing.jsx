36f2af1812d022ec8e3860d3d18ea6a639c60a22
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-28 10:14:27 -0500

    Added Navbar.css and adjusted HTML

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,18 @@
+//src/components/Landing/Landing.jsx
+import './Landing.css'; 
+import { Link } from 'react-router-dom'; 
+import mapIcon from '../../assets/Moment-Map-Icon-inverse.png';
+
 const Landing = () => {
   return (
-    <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-      <h3>
-        If you sign up for a new account, you will have the ability to sign in
-        and see your super secret dashboard.
-      </h3>
+    <main className="landing-container">
+      <h1 className="landing-title">Welcome to Moment Map</h1>
+      <div className="landing-image">
+        <img src={mapIcon} alt="Map Icon" />
+      </div>
+      <p className="landing-text">
+        <Link to="/signup" className="landing-link">Sign up</Link> or <Link to="/signin" className="landing-link">Sign in</Link> to track your moments
+      </p>
     </main>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,40 +2,48 @@
 import { Link } from 'react-router-dom';
 import { AuthedUserContext } from '../../App';
 import { useContext } from 'react';
+import './NavBar.css';
+import logo from '../../assets/Moment-Map-Icon-inverse.png';
 
 const NavBar = ({ handleSignout }) => {
   const user = useContext(AuthedUserContext);
+
   return (
-    <>
-      {user ? (
-        <nav>
-          <ul>
-            <li>Welcome, {user.username}</li>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Moment Map Logo" />
+        <Link to="/" className="navbar-brand">Moment Map</Link>
+      </div>
+      <ul className="navbar-links">
+        {user && (
+          <>
             <li>
-              <Link to="/">Dashboard</Link>
+              <Link to="/moments">Moments</Link>
             </li>
-            <li><Link to='/moments'>Moments</Link></li>
             <li>
-            <li><Link to='/calendar'>Calendar</Link></li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
+              <Link to="/calendar">Calendar</Link>
+            </li>
+            <li className="navbar-user">Welcome, {user.username}</li>
+            <li>
+              <Link to="/" onClick={handleSignout} className="navbar-signout">
+                Sign out
               </Link>
             </li>
-          </ul>
-        </nav>
-      ) : (
-        <nav>
-          <ul>
+          </>
+        )}
+        {!user && (
+          <>
             <li>
               <Link to="/signin">Sign In</Link>
             </li>
             <li>
               <Link to="/signup">Sign Up</Link>
             </li>
-          </ul>
-        </nav>
-      )}
-    </>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 };
+
 export default NavBar;
